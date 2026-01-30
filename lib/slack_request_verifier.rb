f728ba2bacb3f4c69b4ceb48f0ed62c75b847e3a
Alessio Signorini <alessio@signorini.us>
2026-01-30 16:15:11 +0000

    Add SlackRequestVerifier (REQ-030)

---
+++ lib/slack_request_verifier.rb
@@ -0,0 +1,66 @@
+require 'openssl'
+require 'rack/utils'
+require_relative './errors'
+
+module SlackMeet
+  # Verifies Slack request signatures to ensure requests are authentic.
+  #
+  # @example
+  #   SlackRequestVerifier.verify!(request, signing_secret: ENV['SLACK_SIGNING_SECRET'])
+  #
+  class SlackRequestVerifier
+    # Maximum age of a request in seconds (5 minutes)
+    MAX_REQUEST_AGE = 5 * 60
+
+    # Verify a Slack request signature
+    #
+    # @param request [Rack::Request] The request to verify
+    # @param signing_secret [String] Slack signing secret
+    # @raise [SlackVerificationError] If verification fails
+    # @return [void]
+    def self.verify!(request, signing_secret:)
+      timestamp = request.env['HTTP_X_SLACK_REQUEST_TIMESTAMP']
+      signature = request.env['HTTP_X_SLACK_SIGNATURE']
+      
+      raise Errors::SlackVerificationError, 'Missing timestamp header' unless timestamp
+      raise Errors::SlackVerificationError, 'Missing signature header' unless signature
+      
+      verify_timestamp!(timestamp)
+      verify_signature!(request.body.read, timestamp, signature, signing_secret)
+      
+      # Reset body for subsequent reads
+      request.body.rewind
+    end
+
+    # Verify the timestamp is within acceptable age
+    #
+    # @param timestamp [String] Request timestamp
+    # @raise [SlackVerificationError] If timestamp is too old
+    # @return [void]
+    def self.verify_timestamp!(timestamp)
+      request_time = Time.at(timestamp.to_i)
+      age = Time.now - request_time
+      
+      if age.abs > MAX_REQUEST_AGE
+        raise Errors::SlackVerificationError, 'Request timestamp too old'
+      end
+    end
+
+    # Verify the HMAC signature
+    #
+    # @param body [String] Request body
+    # @param timestamp [String] Request timestamp
+    # @param signature [String] Slack signature
+    # @param signing_secret [String] Slack signing secret
+    # @raise [SlackVerificationError] If signature doesn't match
+    # @return [void]
+    def self.verify_signature!(body, timestamp, signature, signing_secret)
+      sig_basestring = "v0:#{timestamp}:#{body}"
+      computed_signature = 'v0=' + OpenSSL::HMAC.hexdigest('SHA256', signing_secret, sig_basestring)
+      
+      unless Rack::Utils.secure_compare(computed_signature, signature)
+        raise Errors::SlackVerificationError, 'Invalid signature'
+      end
+    end
+  end
+end
---
+++ test/requirements/slack/req_030_signature_verification_test.rb
@@ -0,0 +1,95 @@
+require_relative '../../test_helper'
+require_relative '../../../lib/slack_request_verifier'
+require 'rack'
+require 'openssl'
+require 'stringio'
+
+class Req030SignatureVerificationTest < Minitest::Test
+  def setup
+    @signing_secret = 'test_signing_secret'
+    @timestamp = Time.now.to_i.to_s
+    @body = 'token=test&team_id=T123&user_id=U123&command=/meet&text='
+  end
+
+  def test_valid_signature_passes
+    request = build_request(@body, @timestamp, @signing_secret)
+    
+    # Should not raise
+    SlackMeet::SlackRequestVerifier.verify!(request, signing_secret: @signing_secret)
+  end
+
+  def test_invalid_signature_raises_error
+    request = build_request(@body, @timestamp, 'wrong_secret')
+    
+    error = assert_raises(SlackMeet::Errors::SlackVerificationError) do
+      SlackMeet::SlackRequestVerifier.verify!(request, signing_secret: @signing_secret)
+    end
+    
+    assert_match(/Invalid signature/, error.message)
+  end
+
+  def test_expired_timestamp_raises_error
+    old_timestamp = (Time.now - (6 * 60)).to_i.to_s  # 6 minutes ago
+    request = build_request(@body, old_timestamp, @signing_secret)
+    
+    error = assert_raises(SlackMeet::Errors::SlackVerificationError) do
+      SlackMeet::SlackRequestVerifier.verify!(request, signing_secret: @signing_secret)
+    end
+    
+    assert_match(/too old/, error.message)
+  end
+
+  def test_future_timestamp_raises_error
+    future_timestamp = (Time.now + (6 * 60)).to_i.to_s  # 6 minutes in future
+    request = build_request(@body, future_timestamp, @signing_secret)
+    
+    error = assert_raises(SlackMeet::Errors::SlackVerificationError) do
+      SlackMeet::SlackRequestVerifier.verify!(request, signing_secret: @signing_secret)
+    end
+    
+    assert_match(/too old/, error.message)
+  end
+
+  def test_missing_timestamp_header_raises_error
+    env = {
+      'HTTP_X_SLACK_SIGNATURE' => 'v0=signature',
+      'rack.input' => StringIO.new(@body)
+    }
+    request = Rack::Request.new(env)
+    
+    error = assert_raises(SlackMeet::Errors::SlackVerificationError) do
+      SlackMeet::SlackRequestVerifier.verify!(request, signing_secret: @signing_secret)
+    end
+    
+    assert_match(/Missing timestamp/, error.message)
+  end
+
+  def test_missing_signature_header_raises_error
+    env = {
+      'HTTP_X_SLACK_REQUEST_TIMESTAMP' => @timestamp,
+      'rack.input' => StringIO.new(@body)
+    }
+    request = Rack::Request.new(env)
+    
+    error = assert_raises(SlackMeet::Errors::SlackVerificationError) do
+      SlackMeet::SlackRequestVerifier.verify!(request, signing_secret: @signing_secret)
+    end
+    
+    assert_match(/Missing signature/, error.message)
+  end
+
+  private
+
+  def build_request(body, timestamp, signing_secret)
+    sig_basestring = "v0:#{timestamp}:#{body}"
+    signature = 'v0=' + OpenSSL::HMAC.hexdigest('SHA256', signing_secret, sig_basestring)
+    
+    env = {
+      'HTTP_X_SLACK_REQUEST_TIMESTAMP' => timestamp,
+      'HTTP_X_SLACK_SIGNATURE' => signature,
+      'rack.input' => StringIO.new(body)
+    }
+    
+    Rack::Request.new(env)
+  end
+end
