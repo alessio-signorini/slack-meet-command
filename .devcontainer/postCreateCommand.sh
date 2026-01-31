b5257bc52493e54ee32d13ef6635768a3ba455d5
Alessio Signorini <alessio@signorini.us>
2026-01-31 04:03:12 +0000

    Refactor: Move postCreateCommand to separate shell script

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo "Installing Ruby dependencies..."
+bundle install
+
+echo "Running database migrations..."
+bundle exec rake db:migrate
+
+echo "Installing Fly CLI..."
+curl -L https://fly.io/install.sh | sh
+
+echo "Configuring Fly CLI in PATH..."
+echo 'export FLYCTL_INSTALL="/home/vscode/.fly"' >> ~/.bashrc
+echo 'export PATH="$FLYCTL_INSTALL/bin:$PATH"' >> ~/.bashrc
+
+echo "Setup complete!"
